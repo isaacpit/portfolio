813d811f10a5729f6bf146a829398942e802e0ca
isaacpitblado <isaacpitblado@tamu.edu>
2025-01-25 19:42:16 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-isaacpit.com/precache-manifest.b498b0b2f22789615adf8abe2858df74.js"
+  "/portfolio/precache-manifest.2fba9b3058654b73c857f9ec48d21eee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-isaacpit.com/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
